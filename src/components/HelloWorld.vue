724eb71a7653ed168f07a7a079ff468e9f8ae07a
yang.yang <986699180@qq.com>
2021-03-08 21:31:05 +0800

    搭建脚手架。

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="columns is-mobile" v-for="item in 10" :key="item">
+      <div class="column is-one-quarter list-item">
+        <code>is-one-quarter</code>
+      </div>
+      <div class="column list-item">Auto</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.hello {
+}
+</style>
