c671b3befdf4fd7c03433db7bf6219b02373ae2c
yang.yang <986699180@qq.com>
2021-03-10 19:40:42 +0800

    规范文件命名

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/mainPage.vue';
 
 export default {
   name: 'App',
@@ -14,13 +14,3 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
